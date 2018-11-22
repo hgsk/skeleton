a188e0fa6e7b355e571552f3b66b560d160bcd71
Ryosuke Hagihara <r-hagihara@infiniteloop.co.jp>
2018-11-22 17:57:44 +0900

    .gitignore is now working

---
+++ Assets/ScrollSystem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollSystem : MonoBehaviour {
+    public float speed = 0.1f;
+    public float force = 10f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void FixedUpdate () {
+        Vector3 dir = Vector3.zero;
+        
+        dir.x = Input.GetAxis("Horizontal");
+        dir.z = Input.GetAxis("Vertical");
+        
+        GetComponent<Rigidbody2D>().AddForce(dir * force);
+
+        /* Touch
+            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) {
+                Debug.Log("foo");
+                Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+                transform.Translate(-touchDeltaPosition.x * speed, -touchDeltaPosition.y * speed, 0);
+            }
+        */
+    }
+}
